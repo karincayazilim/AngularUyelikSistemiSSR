1b68bcbbd9791055d10ead8a9c8a1fd3bfd8d80c
Abdulkerim KELEŞ <abdulkerim.keles@turna.tech>
2020-06-03 18:01:59 +0300

    auth login and forgot password screen add

--- src/app/content/pages/auth/auth.component.ts
+++ src/app/content/pages/auth/auth.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, AfterViewInit} from '@angular/core';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.less']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, AfterViewInit {
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.onResize(null);
+    }, 50);
+  }
+  onResize(event) {
+    const height = $("#slider").height();
+    $(".bc").css('height', height);
   }
 
 }
