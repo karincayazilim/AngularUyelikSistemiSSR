9ef1c016597a2855f79d81c636c00a0d871638b5
abdulkerim.keles <abdulkerim.keles@turna.tech>
2020-06-22 09:06:46 +0300

    edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Route} from '@angular/router';
-import {AuthComponent} from "./content/pages/auth/auth.component";
-import {LoginComponent} from "./content/pages/auth/login/login.component";
-import {RegisterComponent} from "./content/pages/auth/register/register.component";
-import {ForgotPasswordComponent} from "./content/pages/auth/forgot-password/forgot-password.component";
-import {MainPageComponent} from "./content/pages/main-page/main-page.component";
-import {ProfileComponent} from "./content/pages/main-page/profile/profile.component";
-import {SettingComponent} from "./content/pages/main-page/setting/setting.component";
-import {DashboardComponent} from "./content/pages/main-page/dashboard/dashboard.component";
+import {AuthComponent} from './content/pages/auth/auth.component';
+import {LoginComponent} from './content/pages/auth/login/login.component';
+import {RegisterComponent} from './content/pages/auth/register/register.component';
+import {ForgotPasswordComponent} from './content/pages/auth/forgot-password/forgot-password.component';
+import {MainPageComponent} from './content/pages/main-page/main-page.component';
+import {ProfileComponent} from './content/pages/main-page/profile/profile.component';
+import {SettingComponent} from './content/pages/main-page/setting/setting.component';
+import {DashboardComponent} from './content/pages/main-page/dashboard/dashboard.component';
 
 
 export const appRoutes: Route[] = [
-  { path: '', redirectTo: '/auth/login', pathMatch: 'full'},//url adresi boşsa logine yönlendir
-  { //herkese açık sayfalardır. Public pages
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full'}, // url adresi boşsa logine yönlendir
+  { // herkese açık sayfalardır. Public pages
     path: 'auth', component: AuthComponent, children: [
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
       { path: 'forgot-password', component: ForgotPasswordComponent }
     ]
   },
-  { //üye girişi yapan kullanıcıların erişebileceği sayfalardır. Private pages
+  { // üye girişi yapan kullanıcıların erişebileceği sayfalardır. Private pages
     path: 'page', component: MainPageComponent, canActivateChild: [], children: [
       { path: 'dashboard', component: DashboardComponent },
       { path: 'profiles', component: ProfileComponent },
